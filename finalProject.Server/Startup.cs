9b41c1f18e2ea3196a5440c9687473706494770b
Payton Macdonald <paytonmacdonald49@gmail.com>
2021-05-27 16:19:43 -0600

    second commit

--- finalProject.Server/Startup.cs
+++ finalProject.Server/Startup.cs
@@ -1,7 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
+// using finalProject.Repositories;
+// using finalProject.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -11,6 +15,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using MySqlConnector;
 
 namespace finalProject.Server
 {
@@ -26,12 +31,64 @@ namespace finalProject.Server
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            ConfigureCors(services);
+            ConfigureAuth(services);
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "finalProject.Server", Version = "v1" });
             });
+
+            services.AddScoped<IDbConnection>(x => CreateDbConnection());
+
+            // services
+            // services.AddScoped<AccountService>();
+            // services.AddTransient<ValuesService>();
+            // repositories
+            // services.AddScoped<AccountsRepository>();
+            // services.AddTransient<ValuesRepository>();
+
+
+        }
+
+        private void ConfigureCors(IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsDevPolicy", builder =>
+                {
+                    builder
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials()
+                    .WithOrigins(new string[]{
+                        "http://localhost:8080", "http://localhost:8081"
+                    });
+                });
+            });
+        }
+
+        private void ConfigureAuth(IServiceCollection services)
+        {
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                // NOTE this must match the object structure in appsettings.json
+                options.Authority = $"https://{Configuration["Auth0:Domain"]}/";
+                options.Audience = Configuration["Auth0:Audience"];
+            });
+
+        }
+
+        private IDbConnection CreateDbConnection()
+        {
+            // NOTE this must match the object structure in appsettings.json
+            string connectionString = Configuration["DB:Connectionstring"];
+            return new MySqlConnection(connectionString);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,11 +99,18 @@ namespace finalProject.Server
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "finalProject.Server v1"));
+                app.UseCors("CorsDevPolicy");
             }
 
             app.UseHttpsRedirection();
 
+            // NOTE use to serve your built client
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
