96d6b22d1c3b77448879bc28251da96895bfec50
Payton Macdonald <paytonmacdonald49@gmail.com>
2021-05-27 15:55:59 -0600

    first commit

---
+++ finalProject.Client/src/env.js
@@ -0,0 +1,5 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:3000' : ''
+export const domain = 'payton49.us.auth0.com'
+export const audience = 'https://finalproject'
+export const clientId = 'dXUQWGuPQ5e2iYOi9vbBaxf1J0AA4bt7'
